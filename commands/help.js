6c652deef147483e5da4010e4403b005f6f2d97e
abdurrohimbontro <89439439+abdurrohimbontro@users.noreply.github.com>
2021-10-05 18:31:17 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,13 +2,13 @@ module.exports = (client) => {
   return `
 Commands
 \`\`\`
-@${client.user.username} mode <channel|dm|off>: turn mentions on or off for your user and choose method
-@${client.user.username} name <username|nickname|both|off>: turn on mentions for your names
-@${client.user.username} add <1>, <2>, <3>: add phrases that you should be mentioned for
-@${client.user.username} remove <1>, <2>, <3>: remove phrases that you should be mentioned for
-@${client.user.username} list: list your settings and added mention phrases
-@${client.user.username} channel #<channel name>: set channel for mentions to appear in (admin only)
-@${client.user.username} init: Manually initialize server if bot isn't working (admin only)
-@${client.user.username} help: display available commands\`\`\`
+@${client.user.username} mode <channel|dm|off>: aktifkan atau nonaktifkan sebutan untuk pengguna Anda dan pilih metode
+@${client.user.username} name <username|nickname|both|off>: aktifkan sebutan untuk nama Anda
+@${client.user.username} tambahkan <1>, <2>, <3>: tambahkan frasa yang harus Anda sebutkan
+@${client.user.username} hapus <1>, <2>, <3>: hapus frasa yang harus Anda sebutkan
+@${client.user.username} list: buat daftar pengaturan Anda dan tambahkan frasa penyebutan
+@${client.user.username} channel #<channel name>: setel channel agar sebutan muncul (khusus admin)
+@${client.user.username} init: Inisialisasi server secara manual jika bot tidak berfungsi (hanya admin)
+@${client.user.username} bantuan: menampilkan perintah yang tersedia\`\`\`
 `
 }
