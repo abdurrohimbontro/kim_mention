0467aa2d3fbac8d79593bc5cccb2bb5cdc27f534
abdurrohimbontro <89439439+abdurrohimbontro@users.noreply.github.com>
2021-10-05 18:52:02 +0700

    Update name.js

--- commands/name.js
+++ commands/name.js
@@ -3,8 +3,8 @@ const db = require('../src/db')
 module.exports = async (message, arg) => {
   if (['username', 'nickname', 'both', 'off'].includes(arg)) {
     await db.setNameOpt(message.guild.id, message.member, arg)
-    return `Name mentions mode changed to ${arg}`
+    return `Mode penyebutan nama diubah menjadi ${arg}`
   }
 
-  return 'Invalid option. Usage: `@mentionbot name <username|nickname|both|off>`'
+  return 'Invalid option. Usage: `@kimabsenbot name <username|nickname|both|off>`'
 }
