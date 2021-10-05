d623ba878708ca1d0b8a282dff0ff04a1dd46552
abdurrohimbontro <89439439+abdurrohimbontro@users.noreply.github.com>
2021-10-05 18:43:09 +0700

    Update channel.js

--- commands/channel.js
+++ commands/channel.js
@@ -3,7 +3,7 @@ const db = require('../src/db')
 
 module.exports = async (message, args) => {
   if (!message.member.hasPermission(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
-    return 'You must be an admin to change the mention channel'
+    return 'Anda harus menjadi admin untuk mengubah saluran sebutan'
   }
 
   let channel = args.match(/<#(\d+)>/)
@@ -11,9 +11,9 @@ module.exports = async (message, args) => {
     channel = message.guild.channels.get(channel[1])
     if (channel) {
       await db.setChannel(message.guild.id, channel.id)
-      return `Mention channel set to <#${channel.id}>`
+      return `Sebutkan saluran yang disetel ke <#${channel.id}>`
     }
   }
 
-  return 'Invalid channel. Usage: `@mentionbot channel #<channel name>`'
+  return 'Invalid channel. Usage: `@kimabsenbot Channel #<nama saluran>`'
 }
